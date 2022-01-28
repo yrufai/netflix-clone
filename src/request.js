33ad0da855c777774ca2d33add125ae5443dd1d0
yakubu rufai mahatma <yrufai23@gmail.com>
2022-01-28 01:49:28 +0000

    file recovered

--- src/request.js
+++ src/request.js
@@ -9,6 +9,10 @@ const request = {
 	fetchHororMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
 	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
 	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+	fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+	fetchCrime: `/discover/movie?api_key=${API_KEY}&with_genres=80`,
+	fetchWar: `/discover/movie?api_key=${API_KEY}&with_genres=10752`,
+	fetchHistory: `/discover/movie?api_key=${API_KEY}&with_genres=36`,
 };
 
 export default request;
